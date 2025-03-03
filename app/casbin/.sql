500a671284d974b512f316d9625bcedb554fe1a5
chenghao he <hch20041214sr@qq.com>
2025-03-03 16:12:01 +0800

    Delete app/casbin directory

--- app/casbin/.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO casbin_rule (ptype, v0, v1, v2) VALUES ('p', 'checkout', 'place_order', 'use');
-INSERT INTO casbin_rule (ptype, v0, v1, v2) VALUES ('p', 'checkout', 'mark_order_paid', 'use');
-INSERT INTO casbin_rule (ptype, v0, v1, v2) VALUES ('p', 'checkout', 'empty_cart', 'use');
-INSERT INTO casbin_rule (ptype, v0, v1, v2) VALUES ('p', 'checkout', 'charge', 'use');
-INSERT INTO casbin_rule (ptype, v0, v1) VALUES ('g', 'checkout', 'checkout');
